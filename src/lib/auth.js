4b312b07e628327c6d0e714ec3c58b0c95a82c60
Mahedi <mahidehasansiyam123@gmail.com>
2026-06-24 19:44:00 +0600

    Home Page

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,14 +2,6 @@ import { betterAuth } from 'better-auth';
 import { MongoClient } from 'mongodb';
 import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 
-// if (!process.env.MONGOBD_URI) {
-//   throw new Error('MONGODB_URI missing');
-// }
-
-// if (!process.env.USER_DB_NAME) {
-//   throw new Error('USER_DB_NAME missing');
-// }
-
 const client = new MongoClient(process.env.MONGOBD_URI);
 const db = client.db('taskhive');
 
