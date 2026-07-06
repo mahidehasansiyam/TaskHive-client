e0247b40604ac1ad1cb568cace216f003e8f1a2b
Mahedi <mahidehasansiyam123@gmail.com>
2026-07-06 18:40:08 +0600

    update

--- src/app/(dashboard)/dashboard/admin/users/page.jsx
+++ src/app/(dashboard)/dashboard/admin/users/page.jsx
@@ -11,7 +11,7 @@ const Users = async () => {
   const users = allUsers.filter(user => user.role !== 'admin');
 
   return (
-    <div className="p-6">
+    <div className="p-1 md:p-6">
       <h1 className="text-2xl text-black font-bold mb-6">Users Management</h1>
 
       <UsersTable users={users} />
